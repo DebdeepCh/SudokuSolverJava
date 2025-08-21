787d22cacf688fe5bf66bcf1121d526394a84384
DebdeepCh <138590142+DebdeepCh@users.noreply.github.com>
2025-08-21 10:48:40 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class SudokuSolver {
+
+    // Print the board
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0 && i != 0) {
+                System.out.println("------+-------+------");
+            }
+            for (int j = 0; j < 9; j++) {
+                if (j % 3 == 0 && j != 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[i][j] == 0 ? ". " : board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Check if number is valid
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check row & column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num)
+                return false;
+        }
+        // Check 3x3 box
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (board[i][j] == num)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    // Backtracking solver
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) { // empty cell
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0; // backtrack
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // solved
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] board = new int[9][9];
+
+        System.out.println("Enter Sudoku puzzle (0 for empty cells) row by row:");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nInput Puzzle:");
+        printBoard(board);
+
+        if (solveSudoku(board)) {
+            System.out.println("\nSolved Sudoku:");
+            printBoard(board);
+        } else {
+            System.out.println("\nNo solution exists for this Sudoku.");
+        }
+
+        sc.close();
+    }
+}
